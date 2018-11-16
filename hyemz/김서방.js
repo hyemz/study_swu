99181a1aa259797e07fb5bb1510107450178cf91
햄찌 <32324290+hyemz@users.noreply.github.com>
2018-11-17 02:20:05 +0900

    김서방_혜민

---
+++ hyemz/김서방.js
@@ -0,0 +1,12 @@
+function solution(seoul) {
+    var answer = '';
+    var location;
+    for (location = 0; location < seoul.length; location++)
+        {
+            if (seoul[location]=="Kim"){
+            answer = '김서방은 ' + location + '에 있다';
+            }
+        }
+    return answer;
+}
+console.log(solution(["Jane", "Yun", "Kim", "Park"])); 
